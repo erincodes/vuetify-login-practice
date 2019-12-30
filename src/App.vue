cfd3c714dc8fb121a9912375aed9742e42790573
Erin <erin@rs21.io>
2019-12-30 12:19:21 -0700

    swapped comment

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <!-- Vuetify login page is accessible and cross-browser supported  -->
+  <!-- Components prefaced with `v-` are vuetify components -->
   <v-app>
-    <!-- Components prefaced with `v-` are vuetify components -->
+    <!-- Vuetify login page is accessible and cross-browser supported  -->
     <v-card width="400" class="mx-auto mt-5">
       <v-card-title>
         <h1 class="display-1">Login</h1>
