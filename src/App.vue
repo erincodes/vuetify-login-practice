957465a288c86e5d72ef32a1ee1f0d6f544207dd
Erin <erin@rs21.io>
2019-12-30 13:24:29 -0700

    refactored routing links to sync header & footer

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,15 @@
     <v-app-bar app dark color="primary">
       <v-toolbar-title>Vuetify Dashboard</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn text rounded to="/">Home</v-btn>
-      <v-btn text rounded to="login">Login</v-btn>
+      <!-- Use ES6 syntax for :key so it's unique from the footer key -->
+      <v-btn
+        text
+        rounded
+        :to="link.url"
+        v-for="link in links"
+        :key="`${link.label}-header-link`"
+        >{{ link.label }}</v-btn
+      >
     </v-app-bar>
     <!-- v-content Vuetify component helps with page layout -->
     <v-content>
@@ -13,14 +20,16 @@
     </v-content>
     <v-footer color="primary lighten-1" padless>
       <v-row justify="center" no-gutters>
+        <!-- Use ES6 syntax for :key so it's unique from the navbar key -->
         <v-btn
           v-for="link in links"
-          :key="link"
+          :key="`${link.label}-footer-link`"
           color="white"
           text
           rounded
           class="my-2"
-          >{{ link }}</v-btn
+          :to="link.url"
+          >{{ link.label }}</v-btn
         >
         <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
           {{ new Date().getFullYear() }} —
@@ -42,7 +51,10 @@ export default {
   },
 
   data: () => ({
-    links: ["Home", "Login"]
+    links: [
+      { label: "Home", url: "/" },
+      { label: "Login", url: "/login" }
+    ]
   })
 };
 </script>
